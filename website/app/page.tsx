71824baeb44cf1d1e7293f84fb471b441e09d546
Ashishjob <ashishjob104@gmail.com>
2023-10-11 23:16:52 -0500

    feat: add responsive view message

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,13 +1,33 @@
 import Image from "next/image";
 import LandingPage from "./landingPage/page";
 import VisualizationPage from "./dataVisualiztionPage/page";
+import { Amaranth } from "next/font/google";
 
+const amaranth = Amaranth({ subsets: ["latin"], weight: "700" });
 
 export default function Home() {
   return (
     <main className="bg-primary min-h-screen pt-40">
-      <LandingPage/>
-      <VisualizationPage/>
+      <div className="hidden lg:block">
+        <LandingPage />
+        <VisualizationPage />
+      </div>
+      <div className="lg:hidden items-center justify-center text-center bg-primary min-h-screen pt-0">
+        <h1>
+          <span className="text-7xl font-bold text-white text-stroke-3 md:hidden">
+            <span className={amaranth.className}>
+              This site is not optimized for mobile view yet, but it will be
+              soon!
+            </span>
+          </span>
+          <span className="text-9xl font-bold text-white text-stroke-3 hidden md:block lg:hidden">
+            <span className={amaranth.className}>
+              This site is not optimized for tablet view yet, but it will be
+              soon!
+            </span>
+          </span>
+        </h1>
+      </div>
     </main>
   );
 }
