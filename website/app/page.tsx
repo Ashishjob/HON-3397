251b030ae275803f7d824f778a9a88557e49adc4
Kaitlin Wood <wood.kaitlin3@gmail.com>
2023-10-13 21:10:50 -0500

    final edits

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -14,19 +14,68 @@ export default function Home() {
       <div className="hidden lg:block">
         <LandingPage />
         <div className="flex flex-row items-center justify-center bg-navbarbg p-12">
-          <Image src="/chemicalStructures.png" alt={"chemical structures"} width={1000} height={1000}></Image>
+          <Image
+            src="/chemicalStructures.png"
+            alt={"chemical structures"}
+            width={1000}
+            height={1000}
+          ></Image>
         </div>
         <div className="flex flex-row items-center justify-center bg-primary p-12">
-          <Image src="/predictors.png" alt={"predictors"} width={1500} height={1500}></Image>
+          <Image
+            src="/predictors.png"
+            alt={"predictors"}
+            width={1500}
+            height={1500}
+          ></Image>
         </div>
         <div className="flex flex-row items-center justify-center bg-navbarbg pt-12">
-          <Image src="/scatterPlot.png" alt={"predictors"} width={2500} height={1500}></Image>
+          <Image
+            src="/scatterPlot.png"
+            alt={"predictors"}
+            width={2500}
+            height={1500}
+          ></Image>
         </div>
         <div className="flex flex-row items-center justify-center bg-primary pt-12">
-          <Image src="/MLinPY.png" alt={"predictors"} width={2500} height={1500}></Image>
+          <Image
+            src="/MLinPY.png"
+            alt={"predictors"}
+            width={2500}
+            height={1500}
+          ></Image>
         </div>
         <div className="flex flex-row items-center justify-center bg-navbarbg pt-12">
-          <Image src="/regressionKFold.png" alt={"predictors"} width={2500} height={1500}></Image>
+          <Image
+            src="/regressionKFold.png"
+            alt={"predictors"}
+            width={2500}
+            height={1500}
+          ></Image>
+        </div>
+        <div className="flex flex-row items-center justify-center bg-navbarbg pt -12">
+          <Image
+            src="/metrics.png"
+            alt={"predictors"}
+            width={2500}
+            height={1500}
+          ></Image>
+        </div>
+        <div className="flex flex-row items-center justify-center bg-primary pt -12">
+          <Image
+            src="/trainedLM.png"
+            alt={"predictors"}
+            width={2500}
+            height={1500}
+          ></Image>
+        </div>
+        <div className="flex flex-row items-center justify-center bg-navbarbg pt -12">
+          <Image
+            src="/code.png"
+            alt={"predictors"}
+            width={2500}
+            height={1500}
+          ></Image>
         </div>
       </div>
       <div className="lg:hidden">
